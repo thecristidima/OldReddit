17fcf5aa73b0c66c14da5b06ea517aac123c1de9
Cristi Dima <thecristidima@gmail.com>
2021-01-11 22:54:50 +0200

    Fix a bug with urls that don't contain www.
    Previously, we would only replace the www with old, but this doesn't
    work for urls like https://reddit.com/$path. We now handle both cases.

--- background.js
+++ background.js
@@ -1,20 +1,22 @@
 function redirectToOldReddit(requestDetails) {
-	const url = requestDetails.url;
+    let old_reddit_url = requestDetails.url
+        .replace("://www.reddit.com", "://old.reddit.com")
+        .replace("://reddit.com", "://old.reddit.com"); 
 
-	return {
-		redirectUrl: url.replace("www", "old")
-	};
+    return {
+        redirectUrl: old_reddit_url
+    };
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
-	redirectToOldReddit,
-	{
-		urls: [
-			"http://reddit.com/*",
-			"http://www.reddit.com/*",
-			"https://reddit.com/*",
-			"https://www.reddit.com/*",
-		]
-	},
-	["blocking"]
+    redirectToOldReddit,
+    {
+        urls: [
+            "http://reddit.com/*",
+            "http://www.reddit.com/*",
+            "https://reddit.com/*",
+            "https://www.reddit.com/*",
+        ]
+    },
+    ["blocking"]
 );
